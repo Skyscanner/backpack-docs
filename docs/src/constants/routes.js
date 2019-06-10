db1be5688dd82e873e3099af5313474aec3a8e63
bvitaliyg <bvitaliyg@gmail.com>
2019-06-10 18:26:48 +0100

    [NO-JIRA] Add floating action button page

--- docs/src/constants/routes.js
+++ docs/src/constants/routes.js
@@ -57,6 +57,7 @@ export const BUTTON = '/components/button';
 export const BUTTON_LINK = '/components/button-link';
 export const ICON = '/components/icon';
 export const SPINNER = '/components/spinner';
+export const FLOATING_ACTION_BUTTON = '/components/floating-action-button';
 export const FORM = '/components/form';
 export const CARD = '/components/card';
 export const CHIP = '/components/chip';
