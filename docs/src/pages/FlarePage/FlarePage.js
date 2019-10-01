f8ad6e0d3d5b37c40b67a9f1af3ccda072686852
Ollie Curtis <8831547+ojcurtis@users.noreply.github.com>
2019-10-01 14:23:57 +0100

    Adding flare docs (#132)
    * WIP - Adding flare docs
    
    * Adding the docs for the flare component
    
    * Update docs/src/pages/WebFlarePage/WebFlarePage.js
    
    Co-Authored-By: George Gillams <george.gillams@skyscanner.net>
    
    * Adding the docs for the flare component

---
+++ docs/src/pages/FlarePage/FlarePage.js
@@ -0,0 +1,40 @@
+/*
+ * Backpack - Skyscanner's Design System
+ *
+ * Copyright 2016-2019 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* @flow strict */
+
+import React from 'react';
+
+import DocsPageWrapper from '../../components/DocsPageWrapper';
+import IntroBlurb from '../../components/IntroBlurb';
+import WebFlare from '../WebFlarePage';
+
+const Page = () => (
+  <DocsPageWrapper
+    title="Flare"
+    blurb={[
+      <IntroBlurb>
+        The flare enables you to display content with a pointer to directional
+        content.
+      </IntroBlurb>,
+    ]}
+    webSubpage={<WebFlare wrapped />}
+  />
+);
+
+export default Page;
---
+++ docs/src/pages/WebFlarePage/WebFlarePage.js
@@ -0,0 +1,58 @@
+/*
+ * Backpack - Skyscanner's Design System
+ *
+ * Copyright 2016-2019 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react';
+import BpkText from 'bpk-component-text';
+import { BpkContentBubble } from 'bpk-component-flare';
+import flareReadme from 'bpk-component-flare/README.md';
+import { cssModules } from 'bpk-react-utils';
+
+import DocsPageBuilder from '../../components/DocsPageBuilder';
+
+import STYLES from './Flare.scss';
+
+const getClassName = cssModules(STYLES);
+
+const components = [
+  {
+    id: 'default',
+    title: 'Default',
+    blurb: [],
+    examples: [
+      <div>
+        <BpkContentBubble
+          className={getClassName('bpk-docs-flare')}
+          showPointer
+          rounded={false}
+          content={<BpkText textStyle="xxl">Hello World!</BpkText>}
+        />
+      </div>,
+    ],
+  },
+];
+
+const WebFlarePage = ({ ...rest }) => (
+  <DocsPageBuilder
+    title="Flare"
+    components={components}
+    readme={flareReadme}
+    {...rest}
+  />
+);
+
+export default WebFlarePage;
