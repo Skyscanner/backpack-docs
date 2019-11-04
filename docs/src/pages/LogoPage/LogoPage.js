cd778405fb991925409c627b3baf11e4927c7f52
Ollie Curtis <oliver.curtis@skyscanner.net>
2019-11-04 15:54:58 +0000

    [BPK-3049]: Added logos page

--- docs/src/pages/LogoPage/LogoPage.js
+++ docs/src/pages/LogoPage/LogoPage.js
@@ -23,9 +23,17 @@ import BpkImage from 'bpk-component-image';
 import getMarkdownString from '../../helpers/markdown-helper';
 import BpkMarkdownRenderer from '../../components/DocsPageBuilder/BpkMarkdownRenderer';
 import GuidelinesPageBuilder from '../../components/GuidelinesBuilder';
-import PhotographyHero from '../../static/photography/photography_hero.svg';
-import PhotoArtDirection from '../../static/photography/photography_art_direction.png';
+import LogoHero from '../../static/logo/logo_hero.svg';
+import LogoSymbol from '../../static/logo/our_symbol.png';
+import LogoWordmarque from '../../static/logo/our_wordmarque.svg';
+import LogoExamples from '../../static/logo/logo_examples.png';
+import LogoVertical from '../../static/logo/logo_suite_vertical_logo.svg';
+import LogoHorizontal from '../../static/logo/logo_suite_horizontal_logo.svg';
+import LogoSymbolSuite from '../../static/logo/logo_suite_symbol.svg';
 import * as ROUTES from '../../constants/routes';
+import ContentGrid, {
+  ContentGridCard,
+} from '../../components/ContentGrid/ContentGrid';
 
 import STYLES from './logo-page.scss';
 import intro from './content/intro.md';
@@ -38,16 +46,79 @@ const sections = [
     content: <BpkMarkdownRenderer source={getMarkdownString(intro)} />,
   },
   {
-    id: 'direction',
-    title: 'Art direction',
+    id: 'symbol',
+    title: 'Our symbol',
     backgroundStyle: 'light',
     content: (
       <BpkImage
         className={getClassName('bpk-docs-logo-page__image')}
         altText="art direction"
+        width={758}
+        height={437}
+        src={`/${LogoSymbol}`}
+      />
+    ),
+  },
+  {
+    id: 'wordmarque',
+    title: 'Our wordmarque',
+    content: (
+      <BpkImage
+        className={getClassName('bpk-docs-logo-page__image')}
+        altText="art direction"
         width={838}
-        height={536}
-        src={`/${PhotoArtDirection}`}
+        height={190}
+        src={`/${LogoWordmarque}`}
+      />
+    ),
+  },
+  {
+    id: 'logo-suite',
+    title: 'Logo suite',
+    backgroundStyle: 'light',
+    content: (
+      <ContentGrid>
+        <ContentGridCard className={getClassName('bpk-docs-logo-page__card')}>
+          <img
+            className={getClassName('bpk-docs-logo-page__suite')}
+            src={`/${LogoVertical}`}
+            alt="vertical skyscanner logo with wordmarque"
+          />
+        </ContentGridCard>
+        <ContentGridCard className={getClassName('bpk-docs-logo-page__card')}>
+          <img
+            className={getClassName('bpk-docs-logo-page__suite')}
+            src={`/${LogoHorizontal}`}
+            alt="horizontal skyscanner logo with wordmarque"
+          />
+        </ContentGridCard>
+        <ContentGridCard className={getClassName('bpk-docs-logo-page__card')}>
+          <img
+            className={getClassName('bpk-docs-logo-page__suite')}
+            src={`/${LogoSymbolSuite}`}
+            alt="our symbol, skyscanner logo without wordmarque"
+          />
+        </ContentGridCard>
+        <ContentGridCard className={getClassName('bpk-docs-logo-page__card')}>
+          <img
+            className={getClassName('bpk-docs-logo-page__suite')}
+            src={`/${LogoWordmarque}`}
+            alt="our wordmarque Skyscanner"
+          />
+        </ContentGridCard>
+      </ContentGrid>
+    ),
+  },
+  {
+    id: 'examples',
+    title: 'Examples',
+    content: (
+      <BpkImage
+        className={getClassName('bpk-docs-logo-page__image')}
+        altText="logo examples"
+        width={839}
+        height={838}
+        src={`/${LogoExamples}`}
       />
     ),
   },
@@ -58,7 +129,7 @@ const LogoPage = () => (
     title="Logo"
     hero={{
       heading: `Logo`,
-      imageUrl: `/${PhotographyHero}`,
+      imageUrl: `/${LogoHero}`,
       className: getClassName('bpk-docs-logo-page__hero'),
     }}
     sections={sections}
