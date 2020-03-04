542b69582495fb65c8cf3a93426ce497c180777e
George Gillams <george.gillams@skyscanner.net>
2020-03-04 13:05:13 +0000

    [BPK-3720] Update RN screenshot paths

--- docs/src/pages/NativeNudgerPage/NativeNudgerPage.js
+++ docs/src/pages/NativeNudgerPage/NativeNudgerPage.js
@@ -18,9 +18,9 @@
 
 import React from 'react';
 
-import readme from '../../../../backpack-react-native/packages/react-native-bpk-component-nudger/README.md';
-import iosScreenshotDefault from '../../../../backpack-react-native/packages/react-native-bpk-component-nudger/screenshots/ios/default.png';
-import androidScreenshotDefault from '../../../../backpack-react-native/packages/react-native-bpk-component-nudger/screenshots/android/default.png';
+import readme from '../../../../backpack-react-native/src/js/bpk-component-nudger/README.md';
+import iosScreenshotDefault from '../../../../backpack-react-native/src/js/bpk-component-nudger/screenshots/ios/default.png';
+import androidScreenshotDefault from '../../../../backpack-react-native/src/js/bpk-component-nudger/screenshots/android/default.png';
 import DocsPageBuilder from '../../components/DocsPageBuilder';
 
 const components = [
