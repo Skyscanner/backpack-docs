9e2501039edf2fa6d440d67acf1dd4812b73dd19
Ollie Curtis <8831547+olliecurtis@users.noreply.github.com>
2020-09-11 10:36:01 +0100

    [BPK-4178]: New map markers docs (#679)

--- docs/src/pages/NativeMapPage/NativeMapPage.js
+++ docs/src/pages/NativeMapPage/NativeMapPage.js
@@ -22,6 +22,8 @@ import iosDefaultScreenshot from '../../../../backpack-react-native/lib/bpk-comp
 import androidDefaultScreenshot from '../../../../backpack-react-native/lib/bpk-component-map/screenshots/android/default.png';
 import iosPriceMarkerScreenshot from '../../../../backpack-react-native/lib/bpk-component-map/screenshots/ios/price-markers.png';
 import androidPriceMarkerScreenshot from '../../../../backpack-react-native/lib/bpk-component-map/screenshots/android/price-markers.png';
+import iosIconMarkerScreenshot from '../../../../backpack-react-native/lib/bpk-component-map/screenshots/ios/map-markers.png';
+import androidIconMarkerScreenshot from '../../../../backpack-react-native/lib/bpk-component-map/screenshots/android/map-markers.png';
 import readme from '../../../../backpack-react-native/lib/bpk-component-map/README.md';
 import { NativeComponentPage } from '../../components/ComponentPage';
 
@@ -68,6 +70,30 @@ const components = [
         height: 1920,
         src: `/${androidPriceMarkerScreenshot}`,
         altText: 'Android Map Price Marker Component',
+        subText: '(Google Pixel emulator)',
+      },
+    ],
+  },
+  {
+    id: 'icon-markers',
+    title: 'Icon Markers',
+    blurb:
+      'Use icon markers to display points of interest on a map. By default, they appear in Monteverde. When focused, they grow in size and turn white with a Monteverde border.',
+    screenshots: [
+      {
+        title: 'iOS',
+        width: 750,
+        height: 1334,
+        src: `/${iosIconMarkerScreenshot}`,
+        altText: 'iOS Map Icon Marker Component',
+        subText: '(iPhone 8 simulator)',
+      },
+      {
+        title: 'Android',
+        width: 1080,
+        height: 1920,
+        src: `/${androidIconMarkerScreenshot}`,
+        altText: 'Android Map Icon Marker Component',
         subText: '(Google Pixel emulator)',
       },
     ],
