be53cdd942ccdce4b5b94e09f0702cc73be6db33
Shaun Donnelly <shaundonnelly@gmail.com>
2020-12-04 11:17:17 +0000

    [squash] Small fixes

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -81,17 +81,6 @@ const config = {
         exclude: /node_modules\/(?!bpk-).*/,
       },
       {
-        test: /\.mdx$/,
-        use: [
-          {
-            loader: 'babel-loader',
-          },
-          {
-            loader: '@mdx-js/loader',
-          },
-        ],
-      },
-      {
         test: /base\.scss$/,
         use: [
           {
